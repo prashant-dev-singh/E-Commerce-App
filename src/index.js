6ab6a704be7a9fa5f5fea0524257b0c2a2c92202
prashant-dev-singh <74811257+prashant-dev-singh@users.noreply.github.com>
2021-03-31 17:35:42 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -8,13 +8,9 @@ import Cart from "./Cart";
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    {/* <UseCart.Provider value={{ name: "Dev" }}> */}
-
     <CartProvider>
       <App />
     </CartProvider>
-
-    {/* </UseCart.Provider> */}
   </StrictMode>,
   rootElement
 );
